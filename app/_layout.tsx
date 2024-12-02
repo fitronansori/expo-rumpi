f0e166b032ff65527b1b7e5a8021210381bf0936
fitronansori <fitronansori7@gmail.com>
2024-12-02 12:15:05 +0800

    Set up Tailwind CSS and NativeWind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
+// Import your global CSS file
+import "../global.css";
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
