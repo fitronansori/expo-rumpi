7665eacd2c9d324f27ae824d51c0519d3d1c4c11
fitronansori <fitronansori7@gmail.com>
2024-12-03 10:30:15 +0800

    Add tab navigation layout and screens for Home, Bookmark, Create, and Profile

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,92 @@
+import TabIcon from "@/components/TabIcon";
+import { icons } from "@/constants";
+import { Tabs } from "expo-router";
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 70,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name={"Home"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name={"Bookmark"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name={"Create"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name={"Profile"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
